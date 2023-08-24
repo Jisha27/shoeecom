010a44c840c16120194e45f5f3d85c5fd49e46cf
Jisha27 <jishasumesh27@gmail.com>
2023-08-24 15:16:49 +0530

    mobile css

--- src/Pages/Aboutimg.js
+++ src/Pages/Aboutimg.js
@@ -16,7 +16,9 @@ export default function Aboutimg() {
 All shoes have a sole, which is the bottom of a shoe, in contact with the ground. Soles can be made from a variety of materials, although most modern shoes have soles made from natural rubber, polyurethane, or polyvinyl chloride (PVC) compounds.[53] Soles can be simple—a single material in a single layer—or they can be complex, with multiple structures or layers and materials. When various layers are used, soles may consist of an insole, midsole, and an outsole.[54]
 
 , elastic, velcro straps, buttons, or snaps, for tightening the upper on the foot. Uppers with laces usually have a tongue that helps seal the laced opening and protect the foot from abrasion by the laces. Uppers with laces also have eyelets or hooks to make it easier to tighten and loosen the laces and to prevent the lace from tearing through the upper material. An aglet is the protective wrapping on the end of the lace
+f a shoe, in contact with the ground. Soles can be made from a variety of materials, although most modern shoes have soles made from natural rubber, polyurethane, or polyvinyl chloride (PVC) compounds.[53] Soles can be simple—a single material in a single layer—or they can be complex, with multiple structures or layers and materials. When various layers are used, soles may consist of an insole, midsole, and an outsole.[54]
 
+, elastic, velcro straps, buttons, or snaps, for tightening the upper 
 
            </p>
 
